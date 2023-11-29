0be14d7013e61c993720b99c817739b2846a389b
Monte Yoon <monteyoon1998@gmail.com>
2023-11-28 17:55:36 -0800

    Added styling to the merchant Admin form for a much cleaner look using tailwind css.

--- src/components/MerchantAdmin.jsx
+++ src/components/MerchantAdmin.jsx
@@ -52,50 +52,113 @@ const MerchantAdmin = () => {
         setLocations({...locations, [event.target.name] : event.target.checked})
     }
     return (
-        <div>
-            <fieldset>
-                <form onSubmit={handleCreateUser}>
-                    <fieldset>
+        <div className="flex justify-center items-center">
+            {/* <fieldset className="w-full max-w-lg "> */}
+                <form onSubmit={handleCreateUser} className="w-full max-w-xl ">
+                    <div className="flex justify-center items-center text-lg text-bold underline mb-1">
                         <h3>Create new User for Location</h3>
-                        <div>
-                            First Name: 
-                            <input type="text" value={firstname} onChange={(event) => setFirstname(event.target.value)}required/>
+                    </div>
+                    <div className="flex justify-between mb-6 md:mb-0">
+                        <div className="w-2/5"> 
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                                First Name:
+                            </label> 
+                            <input 
+                                type="text" 
+                                value={firstname} 
+                                onChange={(event) => setFirstname(event.target.value)}
+                                required
+                                className="appearance-none block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight"    
+                            />
                         </div>
-                        <div>
-                            Last Name: 
-                            <input type="text" value={lastname}  onChange={(event) => setLastname(event.target.value)}required/>
+                        <div className="w-1/5 px-4"> 
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                                M. Initials: 
+                            </label>
+                            <input 
+                                type="text"
+                                maxLength={1} 
+                                value={middleInitials.toUpperCase()} 
+                                onChange={(event) => setMiddleInitials(event.target.value)}
+                                className=" block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight"       
+                            />
                         </div>
-                        <div>
-                            Middle Initials: 
-                            <input type="text" maxLength={1} value={middleInitials} onChange={(event) => setMiddleInitials(event.target.value)}/>
+                        <div className="w-2/5">
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                                Last Name: 
+                            </label>
+                            <input 
+                                type="text" 
+                                value={lastname}  
+                                onChange={(event) => setLastname(event.target.value)}
+                                required
+                                className="appearance-none block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight"   
+                            />
                         </div>
-                        <div>
+
+                    </div>
+                    <div className="mb-6 md:mb-0">
+                        <label className="block uppercase tracking-wide text-xs font-bold mb-2">
                             Username: 
-                            <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} required/>
+                        </label>
+                        <input 
+                            type="text" 
+                            value={username} 
+                            onChange={(event) => setUsername(event.target.value)} 
+                            required
+                            className="appearance-none block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight" 
+                        />
+                    </div>
+                    <div className="flex justify-between mb-6 md:mb-0">
+                        <div>
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                                Password: 
+                            </label>
+                            <input 
+                                type="password" 
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)} 
+                                required
+                                className="appearance-none block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight"
+                            />
                         </div>
                         <div>
-                            Password: 
-                            <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} required/>
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                                Confirm Password: 
+                            </label>
+                            <input 
+                                type="password" 
+                                value={passwordConfirm} 
+                                onChange={(event) => setPasswordConfirm(event.target.value)} 
+                                required
+                                className="appearance-none block w-full bg-gray-700 border rounded py-2 px-4 mb-3 leading-tight"
+                            />
                         </div>
-                        <div>
-                            Confirm Password: 
-                            <input type="password" value={passwordConfirm} onChange={(event) => setPasswordConfirm(event.target.value)} required/>
-                        </div>
-                        
-                        <fieldset>
-                            <legend>Select Business Location(s)</legend>
-                            {merchants.map(merchant => 
-                                <div key={merchant.merchant_id}>
-                                    <input  type="checkbox" name={merchant.merchant_id} checked={locations[merchant.merchant_id]} onChange={handleCheckBox}/> 
-                                    <label htmlFor={merchant.street_address1}> {merchant.merchant_name}: {merchant.street_address1} {merchant.state} {merchant.zip_code}</label>
-                                </div>
-                            )}
-                        </fieldset>
-                        <button type="submit">Create</button>
+                    </div>
+                    <fieldset className="mb-6">
+                        <legend>Select Business Location(s)</legend>
+                        {merchants.map(merchant => 
+                            <div key={merchant.merchant_id}>
+                                <input  type="checkbox" name={merchant.merchant_id} checked={locations[merchant.merchant_id]} onChange={handleCheckBox}/> 
+                                <label htmlFor={merchant.street_address1}> {merchant.merchant_name}: {merchant.street_address1} {merchant.state} {merchant.zip_code}</label>
+                            </div>
+                        )}
                     </fieldset>
+                    <button 
+                        type="submit"
+                        className="bg-blue-600"
+                    >
+                        Create
+                    </button>
+                    <button 
+                        onClick={() => navigate("/link-user")}
+                        className="bg-blue-600"
+                    >
+                        Connect Pre-existing user to Business
+                    </button>
                 </form>
-            </fieldset>
-            <button onClick={() => navigate("/link-user")}>Connect Pre-existing user to Business</button>
+            {/* </fieldset> */}
+
         </div>
     )
 }
