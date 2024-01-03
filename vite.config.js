fc24f9621a2c47a29ca92a78836d480f7d4a35cd
apex-devteam <devteam@apexpostech.com>
2024-01-02 16:58:21 -0800

    Changed the target api proxy server URL

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+//ORIGINAL TARGET: "http://localhost:3000/"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: 'http://localhost:3000/',
+        target: 'http://146.190.41.182/',
         changeOrigin: true
       }
     }
